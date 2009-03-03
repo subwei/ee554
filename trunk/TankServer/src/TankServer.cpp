9ffc776643662ebcfe5187719097442cb96ecffb
canichol25 <canichol25@d3c8bccc-06c1-11de-b321-f573fdaff03e>
2009-03-03 20:22:38 +0000

    Added networking functionality to receive messages in the server. Handling the message is yet to come.

--- trunk/TankServer/src/TankServer.cpp
+++ trunk/TankServer/src/TankServer.cpp
@@ -10,6 +10,53 @@
 
 using namespace std;
 
+/******************************************************************************
+ * Receives all of the UDP messages ==> Essentially this is the server
+ *****************************************************************************/
+void RunServer() {
+	int sockfd, length;
+	char buffer[BUFFER_SIZE];
+	struct sockaddr_in server_addr, client_addr;
+
+	cout << "Starting to run the server" << endl;
+
+	/* Create the datagram socket */
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sockfd < 0)
+	{
+		cout << "SERVER ERROR: opening socket" << endl;
+		return;
+	}
+
+	/* Get the servers address */
+	bzero((char *) &server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(SERVER_PORT);
+
+	/* Bind the socket to the address */
+	if(bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{
+		cout << "SERVER ERROR: binding" << endl;
+		return;
+	}
+
+	length = sizeof(struct sockaddr_in);
+	while(true) {
+		int len = recvfrom( sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, (socklen_t *)&length);
+		if(len < 0) cout << "SERVER ERROR: recvfrom" << endl;
+
+		/* Parse the message & save it into the client list if appropriate */
+
+		/* Build the task */
+
+		/* Send the task to the scheduler */
+	}
+}
+
+/******************************************************************************
+ *
+ *****************************************************************************/
 int main() {
 	cout << "Starting the server" << endl;
 	Scheduler *scheduler = new Scheduler();
@@ -32,7 +79,10 @@ int main() {
 		taskHandlerList->at(i)->start();
 	}
 
-	/* Wait for the worker threads to complete */
+	/* begin the server thread */
+	RunServer();
+
+	/* Wait for the worker threads to complete - This should be the last thread */
 //	if( scheduler && scheduler->getThread())
 //		pthread_join( *(scheduler->getThread()), NULL);
 //	for(int i=0; i<taskHandlerList->size(); i++)

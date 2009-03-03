5bbd261adc13935b6a7aeda540f1c1142279c5ce
canichol25 <canichol25@d3c8bccc-06c1-11de-b321-f573fdaff03e>
2009-03-03 19:38:38 +0000

    Initial import.

---
+++ trunk/TankServer/src/Thread.h
@@ -0,0 +1,53 @@
+/*
+ * Thread.h
+ *
+ *  Created on: Mar 3, 2009
+ *      Author: canichols
+ */
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+#include <pthread.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+class Thread {
+private:
+	pthread_t thread;
+	pthread_mutex_t mutex_lock;
+	vector<pthread_cond_t> conditions;
+	int newID;
+
+public:
+	Thread();
+	virtual ~Thread();
+
+	/* Abstract method to carry out the inheriting
+	 * classes functionality */
+	virtual void run() = 0;
+
+	/* Method to start a new thread running */
+	void start();
+
+	/* Method to kill/terminate this thread */
+	void terminate();
+
+	/* Synchronization Methods */
+	void initializeLock();
+	void lock();
+	void unlock();
+
+	/* Condition Variable Methods */
+	int createConditionVar();
+	void destroyConditionVar(int);
+	void wait(int);
+	void signal(int);
+	void broadcast(int);
+};
+
+#endif /* THREAD_H_ */

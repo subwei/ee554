457199eb4ef46cda2e51176a0c4d10f83e136c06
canichol25 <canichol25@d3c8bccc-06c1-11de-b321-f573fdaff03e>
2009-04-15 19:53:21 +0000

    Up through movement is tested for an obstruction free map.

--- ServerTest/src/ServerTest.cpp
+++ ServerTest/src/ServerTest.cpp
@@ -21,8 +21,15 @@ void Register(int sockfd, sockaddr_in to) {
 }
 
 /* Start */
-void Start(int id) {
+void Start(int id, int sockfd, sockaddr_in to) {
+	int len = 2;
+	char buf[2] = "";
+    buf[0] = id;
+    buf[1] = MSG_START;
 
+    /* Send request */
+	if(sendto(sockfd, buf, 2 , 0, (struct sockaddr *)&to, sizeof(to)) == -1)
+		cout << "Failed to register" << endl;
 }
 
 /* Quit */
@@ -36,8 +43,16 @@ void Shoot(int id) {
 }
 
 /* Move */
-void Move(int id) {
+void Move(int id, int sockfd, sockaddr_in to) {
+	int len = 3;
+	char buf[len];
+    buf[0] = id;
+    buf[1] = MSG_MOVE;
+    buf[2] = SOUTH;
 
+    /* Send request */
+	if(sendto(sockfd, buf, len, 0, (struct sockaddr *)&to, sizeof(to)) == -1)
+		cout << "Failed to Move" << endl;
 }
 
 /******************************************************************************
@@ -48,12 +63,11 @@ int main() {
 	int taskID = 0;
 	char buffer[BUFFER_SIZE];
 	struct sockaddr_in server_addr, my_addr;
-	int id1, id2;
+	int id1 = 0, id2 = 0;
 
 	/* Create the datagram socket */
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(sockfd < 0)
-	{
+	if(sockfd < 0) {
 		cout << "SERVER ERROR: opening socket" << endl;
 		return -1;
 	}
@@ -71,8 +85,7 @@ int main() {
 	server_addr.sin_port = htons(SERVER_PORT);
 
 	/* Bind the socket to the address */
-	if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0)
-	{
+	if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
 		cout << "SERVER ERROR: binding" << endl;
 		return -1;
 	}
@@ -94,24 +107,38 @@ int main() {
 
 	/* Wait for the ID */
 	cout << "Player 2 is Registering" << endl;
-	len = recvfrom( sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&my_addr, (socklen_t *)&length);
+	len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&my_addr, (socklen_t *)&length);
 	if(len < 0)	cout << "SERVER ERROR: recvfrom" << endl;
-	id1 = (int)buffer[0];
-	cout << "Player 2 is id # " << id1 << endl;
+	id2 = (int)buffer[0];
+	cout << "Player 2 is id # " << id2 << endl;
 
 	/* Start the game */
+	cout << "Player 1 starts the game" << endl;
+	Start(id1, sockfd, server_addr);
 
 	/* Move player 1 */
+	cout << "Player 1 is moving" << endl;
+	Move(id1, sockfd, server_addr);
 
 	/* Move Player 2 */
+	cout << "" << endl;
+	Move(id2, sockfd, server_addr);
 
 	/* Shoot with player 1 */
+	cout << "" << endl;
+	Shoot(id1);
 
 	/* Shoot with player 2 */
+	cout << "" << endl;
+	Shoot(id2);
 
 	/* Quit with Player 1 */
+	cout << "" << endl;
+	Quit(id1);
 
 	/* Quit with player 2 */
+	cout << "" << endl;
+	Quit(id2);
 
 	return 0;
 }

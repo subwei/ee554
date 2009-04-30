25b457bd6f89557cb7ed5e34e3d3815de910b5de
subwei08 <subwei08@d3c8bccc-06c1-11de-b321-f573fdaff03e>
2009-04-30 05:52:48 +0000

    converted switch statements back into if-else

--- trunk/TankClient/src/GameGUI.java
+++ trunk/TankClient/src/GameGUI.java
@@ -13,44 +13,44 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-public class GameGUI extends JFrame implements KeyListener, ActionListener{
+public class GameGUI extends JFrame implements KeyListener, ActionListener {
     // Width and Length of the game frame
-    public static final int WIDTH   = 800;
-    public static final int LENGTH  = 600;
-    
+    public static final int WIDTH = 800;
+    public static final int LENGTH = 600;
+
     // directions in the game
-    public static final byte NORTH      = (byte)0x01;
-    public static final byte WEST       = (byte)0x02;
-    public static final byte SOUTH      = (byte)0x04;
-    public static final byte EAST       = (byte)0x08;
-    public static final byte NORTHWEST  = (byte)0x10;
-    public static final byte NORTHEAST  = (byte)0x20;
-    public static final byte SOUTHWEST  = (byte)0x40;
-    public static final byte SOUTHEAST  = (byte)0x80;
-    
+    public static final byte NORTH = (byte) 0x01;
+    public static final byte WEST = (byte) 0x02;
+    public static final byte SOUTH = (byte) 0x04;
+    public static final byte EAST = (byte) 0x08;
+    public static final byte NORTHWEST = (byte) 0x10;
+    public static final byte NORTHEAST = (byte) 0x20;
+    public static final byte SOUTHWEST = (byte) 0x40;
+    public static final byte SOUTHEAST = (byte) 0x80;
+
     // Image Icons for the tank
     private ImageIcon northImage;
     private ImageIcon southImage;
     private ImageIcon eastImage;
     private ImageIcon westImage;
-    
+
     // Menu Components
     private JMenuBar menuBar;
     private JMenu menu;
     private JMenuItem register;
-    
+
     private ArrayList<Tank> tanks;
     private TankClient tankClient;
     private Tank tank;
     private byte orientation;
     private ArrayList<JLabel> bullets;
-    
-    public GameGUI(TankClient tankClient){
+
+    public GameGUI(TankClient tankClient) {
         this.tankClient = tankClient;
         setBackground(Color.BLACK);
         tanks = new ArrayList<Tank>();
         bullets = new ArrayList<JLabel>();
-        for (int x=0; x<4; x++){
+        for (int x = 0; x < 4; x++) {
             JLabel bullet = new JLabel("0");
             bullet.setSize(8, 8);
             bullet.setForeground(Color.WHITE);
@@ -64,10 +64,10 @@ public class GameGUI extends JFrame implements KeyListener, ActionListener{
         westImage = new ImageIcon("tank_left.jpg");
         eastImage = new ImageIcon("tank_right.jpg");
         setLayout(null);
-        addKeyListener(this); 
+        addKeyListener(this);
         setupMenu();
     }
-    
+
     private void setupMenu() {
         menuBar = new JMenuBar();
         menu = new JMenu("File");
@@ -77,103 +77,119 @@ public class GameGUI extends JFrame implements KeyListener, ActionListener{
         menuBar.add(menu);
         setJMenuBar(menuBar);
     }
-    
-    public void reset(){
+
+    public void reset() {
         tanks.clear();
     }
-    
-    public void addTank(int x, int y, byte orientation){
-        if (orientation == NORTH){
+
+    public void addTank(int x, int y, byte orientation) {
+        if (orientation == NORTH) {
             tank = new Tank(northImage);
         }
-        else if (orientation == SOUTH){
+        else if (orientation == SOUTH) {
             tank = new Tank(southImage);
         }
-        else if (orientation == EAST){
+        else if (orientation == EAST) {
             tank = new Tank(eastImage);
         }
-        else if (orientation == WEST){
+        else if (orientation == WEST) {
             tank = new Tank(westImage);
         }
-        else{
+        else {
             System.out.println("Error creating Tank: invalid orientation");
         }
         tanks.add(tank);
         tank.moveTo(x, y);
         add(tank);
     }
-    
-    public void drawTank(int tankNum, int x, int y, byte orientation){
-        if (tankNum == tankClient.getClientID()){
+
+    public void drawTank(int tankNum, int x, int y, byte orientation) {
+        if (tankNum == tankClient.getClientID()) {
             this.orientation = orientation;
         }
-        if(tankNum >= tanks.size()) {
-        	System.out.println("Index is larger than the number of registered tanks???");
-        	System.out.println("Tanks Registered: "+tanks.size());
-        	System.out.println("Tank Index: "+tankNum);
-        	return;
+        if (tankNum >= tanks.size()) {
+            System.out
+                    .println("Index is larger than the number of registered tanks???");
+            System.out.println("Tanks Registered: " + tanks.size());
+            System.out.println("Tank Index: " + tankNum);
+            return;
         }
 
-        switch(orientation) {
-        case NORTH:
-//        	System.out.println("North orientation");
-        	tanks.get(tankNum).setIcon(northImage);
-        	break;
-        case SOUTH:
-//        	System.out.println("South orientation");
-        	tanks.get(tankNum).setIcon(southImage);
-        	break;
-        case EAST:
-//        	System.out.println("East orientation");
-        	tanks.get(tankNum).setIcon(eastImage);
-        	break;
-        case WEST:
-//        	System.out.println("West orientation");
-        	tanks.get(tankNum).setIcon(westImage);
-        	break;
-        default:
-        	System.out.println("Unk orientation");
-        	break;
+        if (orientation == NORTH) {
+            tanks.get(tankNum).setIcon(northImage);
         }
+        else if (orientation == SOUTH) {
+            tanks.get(tankNum).setIcon(southImage);
+        }
+        else if (orientation == EAST) {
+            tanks.get(tankNum).setIcon(eastImage);
+        }
+        else if (orientation == WEST) {
+            tanks.get(tankNum).setIcon(westImage);
+        }
+
+        // switch(orientation) {
+        // case NORTH:
+        // // System.out.println("North orientation");
+        // tanks.get(tankNum).setIcon(northImage);
+        // break;
+        // case SOUTH:
+        // // System.out.println("South orientation");
+        // tanks.get(tankNum).setIcon(southImage);
+        // break;
+        // case EAST:
+        // // System.out.println("East orientation");
+        // tanks.get(tankNum).setIcon(eastImage);
+        // break;
+        // case WEST:
+        // // System.out.println("West orientation");
+        // tanks.get(tankNum).setIcon(westImage);
+        // break;
+        // default:
+        // System.out.println("Unk orientation");
+        // break;
+        // }
+
         tanks.get(tankNum).moveTo(x, y);
         invalidate();
         repaint();
         Point p = tanks.get(tankNum).getLocation();
         System.out.println("Point: x " + p.x + " y " + p.y);
-        System.out.println("Draw Tank " + tankNum + ": x " + x + " y " + y + " orient: " + (int)orientation);
+        System.out.println("Draw Tank " + tankNum + ": x " + x + " y " + y
+                + " orient: " + (int) orientation);
     }
-    
-    public void drawBullet(int bulletIndex, int x, int y){
+
+    public void drawBullet(int bulletIndex, int x, int y) {
         bullets.get(bulletIndex).setVisible(true);
-//        System.out.println("x: " + x + " y: " + y);
+        // System.out.println("x: " + x + " y: " + y);
         bullets.get(bulletIndex).setLocation(x, y);
     }
 
     public void keyReleased(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_UP){
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
             tankClient.sendDirectionPacket(NORTH);
-            //tank.moveUp();
+            // tank.moveUp();
         }
-        else if (e.getKeyCode() == KeyEvent.VK_DOWN){
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
             tankClient.sendDirectionPacket(SOUTH);
-            //tank.moveDown();
+            // tank.moveDown();
         }
-        else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             tankClient.sendDirectionPacket(EAST);
-            //tank.moveRight();
+            // tank.moveRight();
         }
-        else if (e.getKeyCode() == KeyEvent.VK_LEFT){
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             tankClient.sendDirectionPacket(WEST);
-            //tank.moveLeft();
+            // tank.moveLeft();
         }
-        else if (e.getKeyCode() == KeyEvent.VK_ENTER){
+        else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             tankClient.startGame();
             System.out.println("Starting Game...");
         }
-        else if (e.getKeyCode() == KeyEvent.VK_SPACE){
+        else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             tankClient.shoot(orientation);
         }
-        else if (e.getKeyCode() == KeyEvent.VK_Q){
+        else if (e.getKeyCode() == KeyEvent.VK_Q) {
             tankClient.quit();
         }
         invalidate();
@@ -181,7 +197,7 @@ public class GameGUI extends JFrame implements KeyListener, ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == register){
+        if (e.getSource() == register) {
             tankClient.createRegistration();
         }
     }
@@ -189,13 +205,9 @@ public class GameGUI extends JFrame implements KeyListener, ActionListener{
     public void keyPressed(KeyEvent e) {
 
     }
-    
+
     public void keyTyped(KeyEvent e) {
         // TODO Auto-generated method stub
-        
-    }   
+
+    }
 }
-
-
-
-
